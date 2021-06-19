9fea893c73407988e0a189d119af403ad98e628b
Ramon van den Akker <ramon.vdakker@gmail.com>
2021-06-19 21:56:54 +0200

    analysis linear regression

---
+++ utils/noise_generators.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import numpy as np
+import pandas as pd
+
+
+def add_noise_laplace_mechanism(data_df: pd.DataFrame, epsilon: float, 
+                                sensitivity: float, seed: int) -> pd.DataFrame:
+    """This function takes a (numerical) dataframe as input and yields a new 
+    dataframe as output where each element arises by adding noise to the 
+    corresponding element from the original dataframe. The Laplace mechanism, 
+    with scale = sensitivity / epsilon > 0, is used to generate the noise. 
+    A seed can be set for reproducubility."""  
+    output_df = data_df.copy()
+    scale = sensitivity / epsilon
+    noise = np.random.default_rng(seed).laplace(0, scale, output_df.shape)
+    output_df = output_df + noise
+    # for testing purposes: variance of Laplace(b) is 2b^2
+    return output_df
+
+
---
+++ utils/simulate.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import pandas as pd
+import numpy as np
+
+def univariate_linear_regression(mu_X, variance_X, alpha, beta, desired_R_squared, n):
+    """This function generated a dataframe with columns Y and X where X consists of i.i.d. draws from a N(mu_X, variance_X)
+    distribution and Y = alpha + beta * X + epsilon. Here epsilon consists of i.i.d. draws from a Logistic distribution with
+    mean zero and the variance is such that the R-squared of the regression is equal to desired_R_squared (in the population)."""
+    # initialize dataframe:
+    data_df = pd.DataFrame(columns=["Y", "X"])
+    # simulate feature / regressor from N(mu_X, variance_X)
+    data_df["X"] = np.random.normal(mu_X, np.sqrt(variance_X), n)
+    # calculate variance epsilon and simulate from Logistic distribution:
+    desired_variance_epsilon = beta ** 2 * variance_X / desired_R_squared - beta ** 2 * variance_X
+    scale = np.sqrt(desired_variance_epsilon * 3) / np.pi
+    data_df["epsilon"] = np.random.logistic(0, scale, n)
+    data_df["Y"] = alpha + beta * data_df["X"] + data_df["epsilon"]
+    data_df.drop(columns="epsilon", inplace=True)
+    return data_df
